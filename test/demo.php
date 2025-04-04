b37e7b0b36992a7e51ec6b854864eb39c97ffa4d
快乐两小时 <1164601554@qq.com>
2025-04-04 11:55:17 +0800

    新增测试样例

--- test/demo.php
+++ test/demo.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 /**
  * 获取类的所有方法信息（包含方法名、标题、注释和类型）
@@ -53,23 +53,89 @@ function getClassMethodsWithComments(string $className): array
     return $result;
 }
 
-use Kingbes\Raylib\Audio;
-use Kingbes\Raylib\Core;
-use Kingbes\Raylib\Gestures;
-use Kingbes\Raylib\Models;
-use Kingbes\Raylib\Text;
-use Kingbes\Raylib\Texture;
-use Kingbes\Raylib\Utils;
+use Kingbes\Raylib\Core; //核心类
+use Kingbes\Raylib\Audio; //音频类
+use Kingbes\Raylib\Shapes; //图形类
+use Kingbes\Raylib\Text; //文本类
+use Kingbes\Raylib\Textures; //纹理类
+use Kingbes\Raylib\Models; //模型类
+use Kingbes\Raylib\Utils; //工具类
 
-$methods = getClassMethodsWithComments(Utils::class);
+$Core = getClassMethodsWithComments(Core::class);
 
-$md = "";
-foreach ($methods as $method) {
-    $md .= "#### {$method['title']}\n\n";
-    $md .= "方法 `{$method['method']}`\n\n";
-    $md .= "```php\n";
-    $md.= "{$method['comment']}\n";
-    $md.= "```\n\n";
+$Core_md = "";
+foreach ($Core as $method) {
+    $Core_md .= "#### {$method['title']}\n\n";
+    $Core_md .= "方法 `{$method['method']}`\n\n";
+    $Core_md .= "```php\n";
+    $Core_md .= "{$method['comment']}\n";
+    $Core_md .= "```\n\n";
 }
 
-file_put_contents('Audio.md', $md);
+file_put_contents(dirname(__DIR__) . '/markdown/Core.md', $Core_md);
+
+$Audio = getClassMethodsWithComments(Audio::class);
+$Audio_md = "";
+foreach ($Audio as $method) {
+    $Audio_md .= "#### {$method['title']}\n\n";
+    $Audio_md .= "方法 `{$method['method']}`\n\n";
+    $Audio_md .= "```php\n";
+    $Audio_md .= "{$method['comment']}\n";
+    $Audio_md .= "```\n\n";
+}
+file_put_contents(dirname(__DIR__) . '/markdown/Audio.md', $Audio_md);
+
+$Shapes = getClassMethodsWithComments(Shapes::class);
+$Shapes_md = "";
+foreach ($Shapes as $method) {
+    $Shapes_md .= "#### {$method['title']}\n\n";
+    $Shapes_md .= "方法 `{$method['method']}`\n\n";
+    $Shapes_md .= "```php\n";
+    $Shapes_md .= "{$method['comment']}\n";
+    $Shapes_md .= "```\n\n";
+}
+file_put_contents(dirname(__DIR__) . '/markdown/Shapes.md', $Shapes_md);
+
+$Text = getClassMethodsWithComments(Text::class);
+$Text_md = "";
+foreach ($Text as $method) {
+    $Text_md .= "#### {$method['title']}\n\n";
+    $Text_md .= "方法 `{$method['method']}`\n\n";
+    $Text_md .= "```php\n";
+    $Text_md .= "{$method['comment']}\n";
+    $Text_md .= "```\n\n";
+}
+file_put_contents(dirname(__DIR__) . '/markdown/Text.md', $Text_md);
+
+$Textures = getClassMethodsWithComments(Textures::class);
+$Textures_md = "";
+foreach ($Textures as $method) {
+    $Textures_md .= "#### {$method['title']}\n\n";
+    $Textures_md .= "方法 `{$method['method']}`\n\n";
+    $Textures_md .= "```php\n";
+    $Textures_md .= "{$method['comment']}\n";
+    $Textures_md .= "```\n\n";
+}
+file_put_contents(dirname(__DIR__) . '/markdown/Textures.md', $Textures_md);
+
+$Models = getClassMethodsWithComments(Models::class);
+$Models_md = "";
+foreach ($Models as $method) {
+    $Models_md .= "#### {$method['title']}\n\n";
+    $Models_md .= "方法 `{$method['method']}`\n\n";
+    $Models_md .= "```php\n";
+    $Models_md .= "{$method['comment']}\n";
+    $Models_md .= "```\n\n";
+}
+file_put_contents(dirname(__DIR__) . '/markdown/Models.md', $Models_md);
+
+$Utils = getClassMethodsWithComments(Utils::class);
+$Utils_md = "";
+foreach ($Utils as $method) {
+    $Utils_md .= "#### {$method['title']}\n\n";
+    $Utils_md .= "方法 `{$method['method']}`\n\n";
+    $Utils_md .= "```php\n";
+    $Utils_md .= "{$method['comment']}\n";
+    $Utils_md .= "```\n\n";
+}
+file_put_contents(dirname(__DIR__) . '/markdown/Utils.md', $Utils_md);
--- test/input_mouse.php
+++ test/input_mouse.php
@@ -5,6 +5,7 @@ include __DIR__ . '/../vendor/autoload.php';
 use Kingbes\Raylib\Core; //核心
 use Kingbes\Raylib\Text; // 文本
 use Kingbes\Raylib\Utils; // 工具类
+use Kingbes\Raylib\Shapes; // 图形
 
 Core::initWindow(800, 450, "input mouse"); //初始化窗口
 
@@ -19,6 +20,8 @@ $green = Utils::Color(0, 255, 0);
 // 是否隐藏光标
 $isCursorHidden  = 0;
 
+$ballColor = Utils::Color(0, 255, 0); // 球的颜色
+
 // 主循环
 while (!Core::windowShouldClose()) {
 
@@ -32,11 +35,30 @@ while (!Core::windowShouldClose()) {
         }
     }
 
+    $ballPosition = Core::getMousePosition(); // 获取鼠标位置
+
+    if (Core::isMouseButtonPressed(0)) { // 左键按下
+        $ballColor = Utils::Color(0, 0, 255); // 改变球的颜色
+    }
+    if (Core::isMouseButtonReleased(0)) { // 左键释放
+        $ballColor = Utils::Color(255, 0, 0); // 恢复球的颜色
+    }
+
     Core::beginDrawing(); //开始绘制
     Core::clearBackground($white); // 清除背景
 
+    // 绘制球
+    Shapes::drawCircleV($ballPosition, 50, $ballColor); // 绘制球
+
     // 绘制文本
-    Text::drawText("按“H”来切换光标的可见性", 10, 30, 20, $green);
+    Text::drawText("Press 'H' to toggle cursor visibility", 10, 30, 20, $green);
+
+    
+    if ($isCursorHidden == 1) { // 如果光标被隐藏
+        Text::drawText("Cursor is hidden", 10, 60, 20, $green);
+    } else {
+        Text::drawText("Cursor is visible", 10, 60, 20, $green);
+    }
 
     Core::endDrawing(); // 结束绘制
 }
