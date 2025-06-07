8a08b766f3088722bfaeb795c7bbd0f4a84f9996
KingBes <48203677+KingBes@users.noreply.github.com>
2025-06-07 11:27:30 +0800

    更新

--- build/webview/mingw64_build.cmd
+++
@@ -1,7 +0,0 @@
-g++ webview.cc ^
--I./webview2/include ^
--Os --std=c++14 -static -DWEBVIEW_STATIC ^
--mwindows -Ilibs ^
--Wl,--gc-sections -ffunction-sections -fdata-sections ^
--ladvapi32 -lole32 -lshell32 -lshlwapi -luser32 -lversion ^
--shared -o webview.a
